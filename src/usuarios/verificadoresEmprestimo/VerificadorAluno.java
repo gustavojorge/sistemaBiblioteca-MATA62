ccf0b0b568d6763e326fe6c55fd1955cf7117b06
gustavojorge <gustavojorge080@gmail.com>
2023-12-05 03:39:03 -0300

    Pequena correção em VerficadorAluno

--- src/usuarios/verificadoresEmprestimo/VerificadorAluno.java
+++ src/usuarios/verificadoresEmprestimo/VerificadorAluno.java
@@ -24,7 +24,7 @@ public class VerificadorAluno implements IVerificadorEmprestimo {
             return false;
         }
 
-        if (VerificadorEmprestimoAtivoLivro.usuarioEValido(usuario, livro)){
+        if (!VerificadorEmprestimoAtivoLivro.usuarioEValido(usuario, livro)){
             return false;
         }
 
